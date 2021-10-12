85f27f6c503067678fbbb6f4bea6600404fae007
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2021-10-12 18:38:25 +0300

    Update Lab4TaskB.cpp
    Изменил способ ввода строки(хотя прошлый коммит мне нравится больше,пусть слова и вводятся через Enter:))

--- Lab4TaskB.cpp
+++ Lab4TaskB.cpp
@@ -2,99 +2,96 @@
 using namespace std;
 int main()
 {
-  string* AS; 
-  string* AS2; 
   int count;
-  string s;
-  char buf[80];
+  const int maxsize=300;
+  char *AS= new char[maxsize];
   cout << "Enter strings:\n";
-  count = 0;
-  AS = nullptr;
-  do
-  {
-    cout << "=> ";
-    cin.getline(buf, 80, '\n'); 
-    s = buf;
-    if (s != "")
-    {
-      count++;
-      AS2 = new string[count];
-      for (int i = 0; i < count - 1; i++)
-      {
-        AS2[i] = AS[i];
-      }
-      AS2[count - 1] = s; 
-      if (AS != nullptr)
-        delete[] AS2;
-      AS = AS2;
-    }
-  } while (s != "");
-  cout << "You array: "<<endl;
-  if (count > 0)
-  {
-    for (int i = 0; i < count; i++)
-    {
-      cout << AS[i] ;
-    }
-    cout<<endl;
-  }
-  cout<<"New array: "<<endl;
   int i = 0;
-  int k = 0;
+  char s;
+  int count= 0;
+  for (int i = 0; s != '.'; i++)
+    {
+        cin.get(s);
+        AS[i] = s;
+    }
+        for (int i = 0; AS[i] != '.'; i++)
+        {
+            count++;
+        }  
+    cout<<"Your string: "<<endl;
+  while(AS[i] !='.' )
+  {
+  cout<<AS[i];
+  i++;
+  }
+  cout<<endl;
+  int a=0;
   int q = 0;
   int cifra = 0;
-  int m = 0;
-  for(int i= 0;i<count;i++)
-  {
-  	 while(AS[i][k] != ' ')
+  int f = 0;
+  cout<<"New string: "<<endl;
+while(f<count) 
+{ 
+  	while(AS[a]!=' ' && AS[a]!='.')
   	{
   		q++;
-  		k++;
+  		a++;
   	}
-  	k=0;
- while(AS[i][k] !=' ')
- {
- 	if(AS[i][k]>47 && AS[i][k]<58)
+  	a=f;
+  	while(AS[a]!=' ')
+  	{
+  			if(AS[a]>47 && AS[a]<58)
  	{
  		cifra++;
  	}
- 	k++;
- }
- if(cifra==q)
- {
- 	cout<<AS[i];
- }
- cifra=0;
- q=0;
- k=0;
-  }
-  int v = 0;
-  int t = 0;
-  int nocifra = 0;
- for(int y = 0;y<count;y++)
- {
- 		 while(AS[y][t] != ' ')
-  	{
-  		v++;
-  		t++;
+ 	a++;
   	}
-  	t=0;
-  	 while(AS[y][t] !=' ')
- {
- 	if(AS[y][t]<48 || AS[y][t]>57)
+  	if(q==cifra)
+  	{
+  		cout<<" ";
+  		for(int w = f;w<q+f;w++)
+  		{
+  			cout<<AS[w];
+  		}  		
+  	} 
+  	cifra= 0;
+  	f=f+q+1;
+  	a=f;  	
+  	q=0;	
+}
+int b=0;
+int r=0;
+int g= 0;
+int necifra = 0;
+while(g<count) 
+{
+  	while(AS[b]!=' ' && AS[b]!='.')
+  	{
+  		r++;
+  		b++;
+  	}
+  	b=g;
+  	while(AS[b]!=' ')
+  	{
+  			if(AS[b]<48 || AS[b]>57)
  	{
- 		nocifra++;
+ 		necifra++;
  	}
- 	t++;
- }
-  if(nocifra==v)
- {
- 	cout<<AS[y];
- }
- nocifra=0;
- v=0;
- t=0;
- }
-  delete[] AS;
-return 0;
+ 	b++;
+  	}
+  	if(r==necifra)
+  	{
+  	cout<<" ";
+  		for(int x = g;x<r+g;x++)
+  		{
+  			cout<<AS[x];
+  		}		
+  	} 
+  	necifra= 0;
+  	g=g+r+1;
+  	b=g;  	
+  	r=0;	
+}
+delete[] AS;
+  return 0;
 }
